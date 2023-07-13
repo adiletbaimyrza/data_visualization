99eb6591de0e2c951a5afcae35410c52ea2d8657
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-07-13 11:34:18 +0200

    range slider values modified according to dataset

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from dash import Dash, dcc, html
+import pandas as pd
+
+df = pd.read_csv('earthquakes.csv')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.RangeSlider(-5, 10, 1, count=1, value=[-3, 7])
+    dcc.RangeSlider(
+        df['mag'].min(),
+        df['mag'].max(), 
+        value=[df['mag'].min(), df['mag'].max()],
+        tooltip={"placement": "bottom", "always_visible": True},
+        id='mag-RangeSlider'
+    ),
+    # dcc.Graph(bla bla bla)
 ])
 
 if __name__ == '__main__':
